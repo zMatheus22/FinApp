cc30974e96bc2f5229c082bd76523712ecc5d888
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2026-04-15 00:44:37 +0000

    adiciona configuração do banco de dados PostgreSQL e endpoints de status

---
+++ backend/src/database/index.js
@@ -0,0 +1,10 @@
+import pg from "pg";
+const { Pool } = pg;
+
+export const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,47 @@
 import Fastify from "fastify";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { pool } from "./database/index.js";
 
 const app = Fastify();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORTBACKEND;
 
 app.get("/", async (request, reply) => {
   return { message: "Server Backend, ON!" };
+});
+
+app.get("/api/v1/status", async (request, reply) => {
+  let status = "OK";
+  try {
+    const databaseNow = await pool.query("SELECT now();");
+    const databaseNowValue = databaseNow.rows[0].now;
+    const databaseVersion = await pool.query("SELECT version();");
+    const databaseVersionValue = databaseVersion.rows[0].version;
+    const databaseMaxConnections = await pool.query("SHOW max_connections;");
+    const databaseMaxConnectionsValue =
+      databaseMaxConnections.rows[0].max_connections;
+
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await pool.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult.rows[0].count;
+
+    return reply.status(200).send({
+      status: status,
+      database: {
+        databaseNow: databaseNowValue,
+        databaseVersion: databaseVersionValue,
+        databaseMaxConnections: databaseMaxConnectionsValue,
+        databaseOpenedConnections: databaseOpenedConnectionsValue,
+      },
+    });
+  } catch (error) {
+    status = "Error";
+  }
+  return reply
+    .status(500)
+    .send({ status: "Error", message: "Erro de conexão com o banco" });
 });
 
 const start = async () => {
